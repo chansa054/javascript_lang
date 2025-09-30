beba52b69d8008c87bfebe1cde70959228d41422
chandra pratap singh deora <chandrapratapsinghdevda@gmail.com>
2025-09-30 20:29:07 +0530

    second

---
+++ data_type.js
@@ -0,0 +1,11 @@
+"use Strict";// treat all js code as newer version 
+// number=>2 to power 53
+// big-int
+// string=> true/false
+// null=>standlone value
+// undefined 
+// symbol=> unique
+// object
+
+let age=23
+console.log(typeof age)
