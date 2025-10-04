110e5fc46c34f3adb0b208ead83bb74824863f05
chandra pratap singh deora <chandrapratapsinghdevda@gmail.com>
2025-10-04 18:33:58 +0530

    conversion

---
+++ data_type_conversion.js
@@ -0,0 +1,7 @@
+let score="33abc"
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(valueInNumber)
+
+// score is not a pure number so it is returning the NaN(not a number may be) if use type of then it show number which is not write
