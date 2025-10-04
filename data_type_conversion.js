9c7feacb68402b3e10efc14e1f5523abba44eef8
chandra pratap singh deora <chandrapratapsinghdevda@gmail.com>
2025-10-04 18:48:57 +0530

    operation

--- data_type_conversion.js
+++ data_type_conversion.js
@@ -1,7 +1,14 @@
 let score="33abc"
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber=Number(score)
-console.log(valueInNumber)
+// console.log(valueInNumber)
 
 // score is not a pure number so it is returning the NaN(not a number may be) if use type of then it show number which is not write
+
+//----------------operation---------------------------------------
+let count=1
+count++
+console.log(count)//read from the mdn dox about the prefix and post fix predcedence 
+++count
+console.log(count)
