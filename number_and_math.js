0997e416466ef1460ae5efcc87b21d7e475201c5
chandra pratap singh deora <chandrapratapsinghdevda@gmail.com>
2025-10-08 21:57:27 +0530

    two files-1.string 2. number and maths

---
+++ number_and_math.js
@@ -0,0 +1,17 @@
+const score =100
+// console.log(typeof score)
+
+const balance = new Number(200)// exclusively converting into numbers through object 
+// console.log(balance.toString().length)
+
+const decimal =123.45
+
+// console.log(decimal.toPrecision(4))// do precision in terms of value
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math.random()*10)// generate the random value
+
+console.log(Math.round(4.6))// round of the number 
+
+// if want to explore more past the code in consoleof browser
---
+++ string.js
@@ -0,0 +1,14 @@
+const name="chansa"
+const repo=4
+
+// console.log(name + repo +" bavji");// we can do like this 
+// console.log(`hello my name is ${name} and my repo is ${repo} and sir name is bavji`);// we use bactics(``)
+const obj = new String("chansa")
+// console.log(obj)// its a object we cna use my methods on it to see the methods run the code in console of browser
+
+// console.log(obj[0])//this is not a array it is a object
+
+// console.log(obj.charAt(2))// character at this position
+// console.log(obj.indexOf('c'))// index of character
+// console.log(obj.length)// length of string object
+const newObj=obj.replace('chan','bham')// first one to be replace and what should be in place
