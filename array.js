e557af8b9ee90a3f82f17dd248f7133740afa3d6
chandra pratap singh deora <chandrapratapsinghdevda@gmail.com>
2025-10-10 22:39:33 +0530

    array

---
+++ array.js
@@ -0,0 +1,15 @@
+const myArr =[1,2,3,4,5]// this is array with the square brakets
+const myArr2=["shaktiman","naagraj"]// we can declare many things
+const myArr3= new Array(1,2,3,4,5,6,7)// this a the another way to decalare the array
+// console.log(myArr[1]);
+
+myArr.push(6);// push at the last
+   
+// many other method also 
+
+// slice and splice method
+myArr.slice(1,4)// this will not manupalte the original array
+console.log(myArr)
+myArr.splice(1,4)// this will manipulate the array
+console.log(myArr)
+
