35ca3858f8d6ff528552e8698fa8497613ca4759
chandra pratap singh deora <chandrapratapsinghdevda@gmail.com>
2025-10-11 22:44:59 +0530

    add on in array

--- array.js
+++ array.js
@@ -9,7 +9,23 @@ myArr.push(6);// push at the last
 
 // slice and splice method
 myArr.slice(1,4)// this will not manupalte the original array
-console.log(myArr)
+// console.log(myArr)
 myArr.splice(1,4)// this will manipulate the array
-console.log(myArr)
+// console.log(myArr)
+
+myArr.push(myArr2)// it will push the whole array at the last index 
+// console.log(myArr)
+// console.log(myArr[2][1])// in this way we can access the element of array in array
+
+console.log(Array.isArray("chansa"))// check if it is a array
+console.log(Array.from("chansa"))// convert to array 
+let score = 100
+let score1= 200
+let score2= 300
+
+console.log(Array.of(score,score1,score2))// convert the variable into array
+
+
+
+
 
